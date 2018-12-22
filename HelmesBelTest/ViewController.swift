a39072de41fef17bfde47c329371e8c17202eb10
Fahrenheit <igor.maisyk@gmail.com>
2018-12-22 11:39:29 +0300

    Initial Commit

---
+++ HelmesBelTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelmesBelTest
+//
+//  Created by Игорь Майсюк on 12/22/18.
+//  Copyright © 2018 Игорь Майсюк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
